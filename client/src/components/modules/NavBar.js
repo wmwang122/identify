ae2f62009c9684730cbf9679979dc128e8c13ab2
William Wang <wmwang@mit.edu>
2022-01-12 00:29:48 -0500

    fixed navbar

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -7,23 +7,24 @@ const NavBar = () => {
   return (
     <div class="nav-list-container">
       <div class="nav-title">Identify</div>
+      <div class="nav-elements">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="howtoplay"> how to play </a>
+          </li>
+        </ul>
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="profile"> profile </a>
+          </li>
+        </ul>
 
-      <ul class="nav-list">
-        <li class="nav-item">
-          <a href="howtoplay"> how to play </a>
-        </li>
-      </ul>
-      <ul class="nav-list">
-        <li class="nav-item">
-          <a href="profile"> profile </a>
-        </li>
-      </ul>
-
-      <ul class="nav-list">
-        <li class="nav-item">
-          <a href="logout"> logout </a>
-        </li>
-      </ul>
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="logout"> logout </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
