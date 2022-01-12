4e4fa94efc39da30a038de9eb700b283b99038df
Jennifer Zhou <jenniferzhou@dhcp-10-31-117-251.dyn.mit.edu>
2022-01-11 22:49:54 -0500

    more navbar stuff e Please enter the commit message for your changes. Lines starting

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -6,10 +6,16 @@ import "./NavBar.css";
 const NavBar = () => {
     return (
       <nav className="NavBar-container">
-        <div className="NavBar-title">identify</div>
-        <div className="NavBar-linkContainer u-inlineBlock">
-          <Link to="/" className="NavBar-link">
-            Home
+        <div className="NavBar-title u-inlineBlock">identify</div>
+        <div className="NavBar-linkContainer u-inlineBlock body">
+          <Link to="/howtoplay" className="NavBar-link NavBar-button">
+            how to play
+          </Link>
+          <Link to="/profile" className="NavBar-link">
+            profile
+          </Link>
+          <Link to="/logout" className="NavBar-link">
+            log out
           </Link>
         </div>
       </nav>
