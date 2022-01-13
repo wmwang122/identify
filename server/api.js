aae90eca4dd1a3473e86e6d33bd0a383faa786aa
William Wang <wmwang@mit.edu>
2022-01-12 20:46:43 -0500

    spotify auth

--- server/api.js
+++ server/api.js
@@ -14,24 +14,69 @@ const User = require("./models/user");
 
 // import authentication library
 const auth = require("./auth");
-
 // api endpoints: all these paths will be prefixed with "/api/"
 const router = express.Router();
-var request = require('request');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-var cors = require('cors');
 
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 //initialize socket
 const socketManager = require("./server-socket");
-var client_id = 'b84e6bd6a87c4f93928c36e67f599187'; // Your client id
-var client_secret = 'ee9a960c35b94c37a22920c96805b12b'; // Your secret
-var redirect_uri = 'http://localhost:5000/api/callback'; // Your redirect uri
 
-router.use(cors());
-router.use(cookieParser());
+const spotifyApi = new SpotifyWebApi({
+  clientId: 'b84e6bd6a87c4f93928c36e67f599187',
+  clientSecret: 'ee9a960c35b94c37a22920c96805b12b',
+  redirectUri: 'http://localhost:5000/api/callback',
+});
+
+router.get('/spotifyLogin', (req, res) => {
+  auth.spotifyLogin(req, res, spotifyApi);
+})
+
+router.get('/callback', async (req, res) => {
+  auth.callback(req, res, spotifyApi);
+});
+
+router.get('/playlists', async (req, res) => {
+  try {
+    const loggedInSpotifyApi = new SpotifyWebApi({
+      clientId: process.env.SPOTIFY_API_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      redirectUri: process.env.CALLBACK_URI,
+    });
+    loggedInSpotifyApi.setRefreshToken(req.user.refreshToken);
+    loggedInSpotifyApi.refreshAccessToken().then(async (data) => {
+      console.log("Access Token Refreshed!");
+      loggedInSpotifyApi.setAccessToken(data.body['access_token']);
+      const result = await loggedInSpotifyApi.getUserPlaylists();
+      res.status(200).send(result.body);
+    })
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+router.get('/getMe', (req, res) => {
+  const loggedInSpotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_API_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.CALLBACK_URI,
+  });
+  loggedInSpotifyApi.setRefreshToken(req.user.refreshToken);
+  loggedInSpotifyApi.refreshAccessToken().then((data) => {
+    loggedInSpotifyApi.setAccessToken(data.body['access_token']);
+    loggedInSpotifyApi.getMe()
+      .then(function (data) {
+        console.log('Some information about the authenticated user', data.body);
+        res.send(data)
+      }, function (err) {
+        console.log('Something went wrong!', err);
+      });
+  });
+})
+
+router.post("/logout", auth.logout);
+
 router.get("/whoami", (req, res) => {
   if (!req.user) {
     // not logged in
@@ -52,118 +97,7 @@ router.post("/initsocket", (req, res) => {
 // |------------------------------|
 
 // anything else falls to this "not found" case
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
-var stateKey = 'spotify_auth_state';
-router.get("/login", function(req, res) {
-
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  var scope = 'user-read-private user-read-email';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-  auth.login;
-});
-router.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
-});
-router.get('/refresh_token', function(req, res) {
-
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
-  });
-});
-router.post("/logout", auth.logout);
 router.all("*", (req, res) => {
   console.log(`API route not found: ${req.method} ${req.url}`);
   res.status(404).send({ msg: "API route not found" });
