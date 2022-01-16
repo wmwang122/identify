3225951cd722ccb3dac3843f09737ef92300a515
Disha Kohli <dishakohli@Dishas-MacBook-Pro.local>
2022-01-16 17:42:05 -0500

    just to make sure things are up to date

--- client/src/components/pages/CreateGame/Game.js
+++ client/src/components/pages/CreateGame/Game.js
@@ -3,9 +3,9 @@ import "../../../utilities.css";
 import "./Game.css";
 import GameCodeInput from "./GameCodeInput.js";
 import CreateGame from "../GameComponents/CreateGame";
-
-/*const Game = (props) => {
-  var optionsButton = props.userId ? (
+import AdvancedOptions from "../GameComponents/AdvancedOptions";
+const Game = (props) => {
+  /*var optionsButton = props.userId ? (
     <>
       <div className="options-button">
         <div className="options-text">Advanced Options</div>
@@ -20,7 +20,7 @@ import CreateGame from "../GameComponents/CreateGame";
     <div className="u-flex u-flex-alignCenter u-flex-alignVertical">
       <div id="Game">
         <div className="game-panel">
-          <div linkclassName="create-game-box">Create a Game</div>
+          <div className="create-game-box">Create a Game</div>
           <div className="join-game-box">
             <div> Join a Game </div>
             <div className="enterGameCode"> enter game code: </div>
